bc45e36ce3448dcbe1171d2587dc721ffe8eb60e
Matt Herbert <mclherbert@gmail.com>
2021-05-26 17:28:36 +1200

    Changed opening page

--- seeds/src/App.js
+++ seeds/src/App.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import { FilteringTable } from './components/FilteringTable';
 import CreateArticle from './components/CreateArticle';
 import ShowArticleList from './components/ShowArticleList';
 import ShowArticleDetails from './components/ShowArticleDetails';
 import UpdateArticleInfo from './components/UpdateArticleInfo';
-import { FilteringTable } from './components/FilteringTable';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route path = '/FilteringTable' component={FilteringTable} />
-          <Route exact path='/' component={ShowArticleList} />
+          <Route exact path='/' component={FilteringTable} />
+          <Route path = '/ShowArticleList' component={ShowArticleList} />
           <Route path='/create-article' component={CreateArticle} />
           <Route path='/edit-article/:id' component={UpdateArticleInfo} />
           <Route path='/show-article/:id' component={ShowArticleDetails} />
